7650118c9e0a184d124ddaa96da17ecbc77e3819
Kishan Suvarna <kishansuvarna09@gmail.com>
2023-06-13 23:54:10 +0400

    Created Dockerfile

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import os
 import psycopg2
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request
 
-CREATE_ROOMS_TABLE = ("CREATE TABEL IF NOT EXISTS rooms (id SERIAL PRIMARY KEY, name TEXT);")
+CREATE_ROOMS_TABLE = ("CREATE TABLE IF NOT EXISTS rooms (id SERIAL PRIMARY KEY, name TEXT);")
 
 CREATE_TEMPS_TABLE = """CREATE TABLE IF NOT EXISTS temperatures (room_id INTEGER, temperature REAL, date TIMESTAMP, FOREIGN KEY(room_id) REFERENCES rooms(id) ON DELETE CASCADE);"""
 
 INSERT_ROOM_RETURN_ID = "INSERT INTO rooms (name) values (%s) RETURNING id;"
+
+INSERT_TEMP = (
+    "INSERT INTO temperatures (room_id, temperature, date) VALUES (%s, %s, %s);"
+)
 
 GLOBAL_NUMBER_OF_DAYS = """SELECT COUNT(DISTINCT DATE(date)) AS days FROM temperature;"""
 
@@ -19,6 +23,18 @@ app = Flask(__name__)
 url = os.getenv("DATABASE_URL")
 connection = psycopg2.connect(url)
 
-@app.get('/')
+@app.route('/')
 def home():
-    return 'Hello World'
+    return 'Site under construction'
+
+@app.post('/api/room')
+def create_room():
+    data = request.get_json()
+    name = data["name"]
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(CREATE_ROOMS_TABLE)
+            cursor.execute(INSERT_ROOM_RETURN_ID, (name,))
+            room_id = cursor.fetchone()[0]
+    return {"id": room_id, "message": f"Room {name} created."}, 201
+    
